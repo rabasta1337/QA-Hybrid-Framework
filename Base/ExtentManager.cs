5e3e2ea062ce69cf6bde9eec868fa663cb416dfc
rabasta1337 <bastiantornado84@gmail.com>
2026-08-15 18:00:57 +0200

    HTML reporting

---
+++ Base/ExtentManager.cs
@@ -0,0 +1,33 @@
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QA_Hybrid_Framework.Base
+{
+    internal class ExtentManager
+    {
+        private static ExtentReports? _extent;
+
+        public static ExtentReports GetInstance()
+        {
+            if (_extent == null)
+            {
+                string reportPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Reports", "TestReport.html");
+                var htmlReporter = new ExtentSparkReporter(reportPath);
+
+                _extent = new ExtentReports();
+                _extent.AttachReporter(htmlReporter);
+                _extent.AddSystemInfo("Environment", "QA");
+                _extent.AddSystemInfo("Framework", "Selenium + RestSharp + NUnit");
+                _extent.AddSystemInfo("Author", "Radosław");
+
+            }
+
+            return _extent;
+        }
+
+
+    }
+}
--- Base/TestBase.cs
+++ Base/TestBase.cs
@@ -1,4 +1,5 @@
-﻿using OpenQA.Selenium;
+﻿using AventStack.ExtentReports;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
@@ -9,18 +10,31 @@ namespace QA_Hybrid_Framework.Base
     public class TestBase
     {
         protected IWebDriver _driver;
+        protected ExtentTest _test = null!;
 
+
+        [OneTimeSetUp]
+        public void BeforeAllTests()
+        {
+            ExtentManager.GetInstance();
+        }
+        
         [SetUp]
         public void SetUp()
         {
+            _test = ExtentManager.GetInstance().CreateTest(TestContext.CurrentContext.Test.Name);
             _driver = new ChromeDriver();
             _driver.Manage().Window.Maximize();
         }
 
         public void TakeScreenshot(string name)
         {
+            string directoryPath = @"C:\Tests";
+            Directory.CreateDirectory(directoryPath);
+
+
             Screenshot ss = ((ITakesScreenshot)_driver).GetScreenshot();
-            string filePath = $@"C:\Tests\Error_{name}.png";
+            string filePath = Path.Combine(directoryPath, $"Error{name}.png");
 
             ss.SaveAsFile(filePath);
             TestContext.Progress.WriteLine($"Screenshot saved in: {filePath}");
@@ -29,14 +43,34 @@ namespace QA_Hybrid_Framework.Base
         [TearDown]
         public void CleanUp()
         {
-            if (TestContext.CurrentContext.Result.Outcome.Status == NUnit.Framework.Interfaces.TestStatus.Failed)
+            var status = TestContext.CurrentContext.Result.Outcome.Status;
+            var errorMessage = TestContext.CurrentContext.Result.Message;
+            string testName = TestContext.CurrentContext.Test.Name;
+
+            if(status == NUnit.Framework.Interfaces.TestStatus.Failed)
             {
-                string testName = TestContext.CurrentContext.Test.Name;
+                var screenshot = ((ITakesScreenshot)_driver).GetScreenshot().AsBase64EncodedString;
+                _test.Fail($"Test failed: {errorMessage}", MediaEntityBuilder.CreateScreenCaptureFromBase64String(screenshot).Build());
+
                 TakeScreenshot(testName);
             }
+            else if (status == NUnit.Framework.Interfaces.TestStatus.Passed)
+            {
+                _test.Pass("Test passed");
+            }
+            else
+            {
+                _test.Skip("Test was skipped");
+            }
 
-           
+            _driver?.Quit();
             _driver?.Dispose();
+        }
+
+        [OneTimeTearDown]
+        public void AfterAllTests()
+        {
+            ExtentManager.GetInstance().Flush();
         }
     }
 }
